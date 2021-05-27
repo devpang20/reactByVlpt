1f9c1ac0c5e37375787a175f0567c4fa2ab4e3c5
sik2 <devpang20@gmail.com>
2021-05-27 13:04:14 +0900

    feat:입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from "./compoents/TodoTemplate";
 import TodoHead from "./compoents/TodoHead";
+import TodoList from "./compoents/TodoList";
+import TodoCreate from './compoents/TodoCreate';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -15,6 +17,8 @@ function App() {
             <GlobalStyle />
             <TodoTemplate>
                 <TodoHead />
+                <TodoList />
+                <TodoCreate />
             </TodoTemplate>
         </>
     );
