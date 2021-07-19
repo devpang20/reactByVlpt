a57aaffcd9720e72857b8c55b9ef4e4df8ff1975
sik2 <devpang20@gmail.com>
2021-07-20 05:58:07 +0900

    feat:function component test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Mycomponent from './MyComponent';
 import Counter from './Counter';
 import EventPractice from './EventPractice';
+import EventPracticeFunc from './EventPracticeFunc';
 
 
 const App = () => {
     return (
         <>
             <EventPractice />
+            <EventPracticeFunc />
             <Mycomponent name="React" favoriteNumber={1}>리엑트</Mycomponent>
             <Counter></Counter>
         </>
