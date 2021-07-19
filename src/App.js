f2296de4967fb331d2b708d68555c92ef100aefa
sik2 <devpang20@gmail.com>
2021-07-20 05:27:16 +0900

    feat:double input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 import Mycomponent from './MyComponent';
 import Counter from './Counter';
 import EventPractice from './EventPractice';
 
-// const GlobalStyle = createGlobalStyle`
-//   body {
-//     background: #e9ecef;
-//   }
-// `;
 
 const App = () => {
     return (
         <>
-            {/* <GlobalStyle />
-            <TodoTemplate>
-                <TodoHead />
-                <TodoList />
-                <TodoCreate />
-            </TodoTemplate> */}
             <EventPractice />
             <Mycomponent name="React" favoriteNumber={1}>리엑트</Mycomponent>
             <Counter></Counter>
