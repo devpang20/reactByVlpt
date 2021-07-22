bdaae31e318058f9271b83baa3fd47dbd6f11c53
sik2 <devpang20@gmail.com>
2021-07-22 23:42:51 +0900

    feat: refactor component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: 'text1',
+            checked: true,
+        },
+        {
+            id: 2,
+            text: 'text2',
+            checked: true,
+        },
+        {
+            id: 3,
+            text: 'text3',
+            checked: true,
+        },
+        {
+            id: 4,
+            text: 'text4',
+            checked: false,
+        }
+
+    ]);
+
     return (
         <TodoTemplate>
             <TodoInsert />
-            <TodoList />
+            <TodoList todos={todos}/>
         </TodoTemplate>
     );
 }
