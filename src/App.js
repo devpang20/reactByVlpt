8f4157786107b37149fa8267c25de2f9721e1742
sik2 <devpang20@gmail.com>
2021-07-26 00:52:02 +0900

    feat: add useRef(), useCallBack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -28,9 +28,24 @@ const App = () => {
 
     ]);
 
+    const nextId = useRef(4);
+
+    const onInsert = useCallback(
+        text => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked: false,
+            };
+            setTodos(todos.concat(todo));
+            nextId.current += 1;
+        },
+        [todos],
+    );
+
     return (
         <TodoTemplate>
-            <TodoInsert />
+            <TodoInsert onInsert={onInsert} />
             <TodoList todos={todos}/>
         </TodoTemplate>
     );
