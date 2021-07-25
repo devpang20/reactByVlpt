c0af6ad4339753f236de8b724ac35a7169dd382c
sik2 <devpang20@gmail.com>
2021-07-26 02:10:10 +0900

    feat:todo app v1 complete

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
 
     const [value, setValue] = useState('');
 
@@ -10,14 +10,30 @@ const TodoInsert = () => {
         setValue(e.target.value);
     }, []);
 
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+            e.preventDefault();
+        }
+    );
+
+    const onClick = useCallback(
+        () => {
+            onInsert(value);
+            setValue('');
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
+        <form className="TodoInsert" onSubmit={onSubmit}>
             <input 
                 placeholder="할 일을 입력하세요"
                 value={value}
                 onChange={onChange}
             />
-            <button type="submit">
+            <button onClick="{onClick}">
                 <MdAdd/>
             </button>
         </form>
