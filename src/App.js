67cdd698a083cc983a73e06d6f86b1c05801de51
sik2 <devpang20@gmail.com>
2021-07-27 04:04:11 +0900

    feat:second components setup

--- src/App.js
+++ src/App.js
@@ -1,70 +1,13 @@
-import React, { useState, useRef, useCallback } from 'react';
-import TodoTemplate from './components/TodoTemplate';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate.js'
+import TodoInsert from './components/TodoInsert.js';
+import TodoList from './components/TodoList.js';
 
 const App = () => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: 'text1',
-            checked: true,
-        },
-        {
-            id: 2,
-            text: 'text2',
-            checked: true,
-        },
-        {
-            id: 3,
-            text: 'text3',
-            checked: true,
-        },
-        {
-            id: 4,
-            text: 'text4',
-            checked: false,
-        }
-
-    ]);
-
-    const nextId = useRef(4);
-
-    const onInsert = useCallback(
-        text => {
-            const todo = {
-                id: nextId.current,
-                text,
-                checked: false,
-            };
-            setTodos(todos.concat(todo));
-            nextId.current += 1;
-        },
-        [todos],
-    );
-
-    const onRemove = useCallback(
-        id => {
-            setTodos(todos.filter(todo => todo.id !== id));
-        },
-        [todos],
-    );
-
-    const onToggle = useCallback(
-        id => {
-            setTodos(
-                todos.map(todo => 
-                 todo.id === id ? {...todo, checked: !todo.checked } : todo,   
-                ),
-            );
-        },
-        [todos],
-    );
-
     return (
         <TodoTemplate>
-            <TodoInsert onInsert={onInsert} />
-            <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+            <TodoInsert />
+            <TodoList />
         </TodoTemplate>
     );
 }
