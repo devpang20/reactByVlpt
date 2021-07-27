f02e2a88016b88a1ac1587ff1f0da7f8f58bc2df
sik2 <devpang20@gmail.com>
2021-07-27 21:56:07 +0900

    reset:reset components

--- src/App.js
+++ src/App.js
@@ -1,66 +1,7 @@
-import React, { useState, useRef, useCallback } from 'react';
-import TodoTemplate from './components/TodoTemplate.js'
-import TodoInsert from './components/TodoInsert.js';
-import TodoList from './components/TodoList.js';
+import React from 'react';
 
 const App = () => {
-
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: 'item1',
-            checked: true,
-        },
-        {
-            id: 2,
-            text: 'item2',
-            checked: true,
-        },
-        {
-            id: 3,
-            text: 'item3',
-            checked: false,
-        },
-        
-    ]);
-
-    const nextId = useRef(4);
-
-    const onInsert = useCallback(
-        text => {
-            const todo = {
-                id: nextId.current,
-                text,
-                checked: false,
-            };
-            setTodos(todos.concat(todo));
-            nextId.current += 1;
-        },
-        [todos],
-    );
-
-    const onRemove = useCallback(
-        id => {
-            setTodos(todos.filter(todo => todo.id !== id));
-        },
-        [todos],
-    )
-
-    const onToggle = useCallback(
-        id => {
-            setTodos(
-                todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo,),
-            );
-        },
-        [todos]
-    )
-
-    return (
-        <TodoTemplate>
-            <TodoInsert onInsert={onInsert} />
-            <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-        </TodoTemplate>
-    );
+    <div>todo-app make</div>
 }
 
 export default App;
