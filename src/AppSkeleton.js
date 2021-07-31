3990496eca0dbf564508299acdeff63e8da379ef
sik2 <devpang20@gmail.com>
2021-08-01 02:48:26 +0900

    feat:add skeleton code for app.js

---
+++ src/AppSkeleton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const App = () => {
+
+    /**
+     * add data
+     * use useState
+     */
+
+    /** 
+     * add id
+     * use useRef
+     */
+
+    /**  
+     *  add onInsert
+     *  use useCallback
+     *  use setTodos
+     */
+
+    /**
+     * add onRemove
+     * use useCallback
+     * use setTodos
+     */
+
+    /**
+     * onToggle
+     * use useCallback
+     * use setTodos
+     */
+
+    return (
+        <div>todo app</div>
+    )
+}
+
+export default App;
